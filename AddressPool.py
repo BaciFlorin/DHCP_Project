bdfa454553e3b80011ade97b46390074d57c854e
Bacica Florin <bacicaflorin@gmail.com>
2019-12-01 16:37:08 +0200

    Adaugare proiect pe master

---
+++ AddressPool.py
@@ -0,0 +1,93 @@
+from IPAddress import *
+
+#de modificat pentru o cautare mai eficienta
+class AddressPool():
+    def __init__(self, _ipAddress, _mask):
+        self.ips = []
+        ip = []
+        invertedMask = []
+        nrIps = 0
+
+        for x in _ipAddress.split('.'):
+            ip.append(int(x))
+
+        for x in _mask.split('.'):
+            invertedMask.append(255-int(x))
+
+        #calculul numarului de ips din spatiu
+        for i in range(0,4):
+            if invertedMask[i] != 0:
+                nrIps += invertedMask[i]*pow(2,8*(3-i))
+
+        #aici se contruieste vectorul in care am toate adresele ip disponibile
+        for i in range (1,nrIps):
+            ip[3] += 1
+            if ip[3] > 255:
+                ip[3] = 0
+                ip[2] += 1
+                if ip[2] > 255:
+                    ip[2] = 0
+                    ip[1] += 1
+                    if ip[1] > 255:
+                        ip[1] = 0
+                        ip[0] += 1
+            self.ips.append(IPAddress(str(ip[0]) + '.' + str(ip[1]) + '.' + str(ip[2]) + '.' + str(ip[3])))
+
+    def getFreeAddress(self,_mac):
+        ip = 0
+        for x in self.ips:
+            if x.free == 1 and x.hold != 1:
+                x.holdAddress()
+                x.setMac(_mac)
+                ip = x
+                break
+        return ip
+
+    def setAddressLock(self,_mac):
+        #functie apelata in caz de clientul doreste acea adresa
+        for x in self.ips:
+            if x.mac == _mac:
+                x.setAddress()
+                break
+
+    def unsetAddressLock(self,_mac):
+        #functie apelata in caz de lease time expira sau clientul trimite mesajul DHCPRELEASE
+        for x in self.ips:
+            if x.mac == _mac:
+                x.unsetAddress()
+                break
+
+    def setAddressUnreserved(self,_mac):
+        #functie apelata in caz de adresa ip atribuita initial nu este validata de client
+        #si astfel o facem disponibila pentru alte atribuiri
+        for x in self.ips:
+            if x.mac == _mac:
+                x.releaseAddress()
+                x.setMac("")
+                break
+
+    def findAddressMAC(self,_mac):
+        ip=0
+        for x in self.ips:
+            if x.mac == _mac:
+                ip = x
+        return ip
+
+    def findAddressIP(self,_ip):
+        ip=0
+        for x in self.ips:
+            if x.ip == _ip:
+                ip = x.ip
+                break
+        if ip==0:
+            #log in care sa precizezi ca nu exista adresa in spatiul ala
+            pass
+        return ip
+
+    def findOldAdress(self, _oldMac):
+        ip = 0
+        for x in self.ips:
+            if x.oldmac == _oldMac:
+                ip = x
+                break
+        return ip
